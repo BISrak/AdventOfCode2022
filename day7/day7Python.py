8e2e4904f1fd43e1b0333eaf36dbed031f111ae1
Phillip <phillipcorporon@hotmail.com>
2025-03-20 11:01:53 -0600

    Day 7 added, havent started

--- day7/day7Python.py
+++ day7/day7Python.py
@@ -1,4 +1,13 @@
 input = open("C:\GitHub\AdventOfCode2022\day7\day7inputsample.txt", "r")
 
 for line in input:
+    # If line contains $ its a command
+    # If line contains numbers followed by text, the numbers are the size of the of the file name, which is the text afterwards
+    # In the readme, it looks like i might want to make an object that builds itself up using the commands to be able to correctly catalog everything. Probably need a 
+    # {
+    # "directoryName":"e", 
+    # "size": 123,
+    #   {"directoryName":"i", "size":123}
+    # }
+    #double check knowledge of making nested objects
     pass
